f719d97a42755b8c75a2ed2e149aae0183cfba97
David DE CARVALHO <dedece35@gmail.com>
2024-02-10 22:41:23 +0100

    nettoyage promethee - OK

--- src/main/java/fr/pagesjaunes/socletechnique/promethee/autoconfigure/PrometheeConfigBootstrapConfiguration.java
+++ src/main/java/fr/pagesjaunes/socletechnique/promethee/autoconfigure/PrometheeConfigBootstrapConfiguration.java
@@ -2,32 +2,28 @@ package fr.pagesjaunes.socletechnique.promethee.autoconfigure;
 
 import fr.pagesjaunes.socletechnique.promethee.PrometheeConfigProperties;
 import fr.pagesjaunes.socletechnique.promethee.PrometheeConfigPropertySourceLocator;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.SearchStrategy;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.task.TaskSchedulerBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
-@ConditionalOnClass(PropertySourceLocator.class)
+//@ConditionalOnClass(PropertySourceLocator.class)
 @EnableConfigurationProperties(PrometheeConfigProperties.class)
 public class PrometheeConfigBootstrapConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(search = SearchStrategy.CURRENT)
     public RestTemplateBuilder restTemplateBuilder() {
-
         return new RestTemplateBuilder();
     }
 
     @Bean
-    @ConditionalOnProperty(prefix = "socletechnique.config.promethee", name = "enabled", matchIfMissing = true)
+//    @ConditionalOnProperty(prefix = "socletechnique.config.promethee", name = "enabled", matchIfMissing = true)
     public PrometheeConfigPropertySourceLocator prometheeConfigPropertySourceLocator(
             final PrometheeConfigProperties configProperties,
             final RestTemplateBuilder restTemplateBuilder) {
