519985e24dba2d633db29c0fb257f7221940ff72
David DE CARVALHO <dedece35@gmail.com>
2024-02-11 21:50:05 +0100

    nettoyage classes BIS - OK

--- src/main/java/fr/pagesjaunes/socletechnique/logging/log4j/stackdriver/Severity.java
+++
@@ -1,6 +0,0 @@
-package fr.pagesjaunes.socletechnique.logging.log4j.stackdriver;
-
-public enum Severity {
-
-    DEBUG, INFO, WARNING, ERROR, DEFAULT
-}
--- src/main/java/fr/pagesjaunes/socletechnique/webmvc/cacheinvalidation/support/XKeyConstants.java
+++
@@ -1,9 +0,0 @@
-package fr.pagesjaunes.socletechnique.webmvc.cacheinvalidation.support;
-
-public class XKeyConstants {
-
-    public static final String XKEY_HEADER_NAME = "xkey";
-
-    private XKeyConstants() {
-    }
-}
